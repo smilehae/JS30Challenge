db2e60376781045a06f94162fbdc91f975e02be6
smilehae <mihaepyun@gmail.com>
2021-08-12 15:21:19 +0900

    프로젝트 11. 최종 완료

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,8 +1,9 @@
 const player = document.querySelector(".player");
 const playBtn = player.querySelector(".toggle");
 const myVideo = player.querySelector("video.viewer");
-const volumeRange = player.querySelector("input[name=volume]");
-const playBackRange = player.querySelector("input[name=playbackRate]");
+// const volumeRange = player.querySelector("input[name=volume]");
+// const playBackRange = player.querySelector("input[name=playbackRate]");
+const ranges = player.querySelectorAll("input[type=range]");
 //이렇게 [속성] 만으로도 찾기 가능
 const skipButtons = player.querySelectorAll("[data-skip]");
 const progress = player.querySelector(".progress");
@@ -28,29 +29,30 @@ playBtn.addEventListener("click", togglePlay);
 myVideo.addEventListener("play", toggleBtn);
 myVideo.addEventListener("pause", toggleBtn);
 
-volumeRange.addEventListener("input", function () {
-  myVideo.volume = this.value;
-});
+//아래 두 개의 함수를 하나의 함수로 합칠 수 있다.
+// volumeRange.addEventListener("input", function () {
+//   myVideo.volume = this.value;
+// });
 
-playBackRange.addEventListener("input", function () {
-  myVideo.playbackRate = this.value;
-});
+// playBackRange.addEventListener("input", function () {
+//   myVideo.playbackRate = this.value;
+// });
+function handleRange() {
+  myVideo[this.name] = this.value;
+}
 
+ranges.forEach((range) => range.addEventListener("change", handleRange));
+
+//0초 전, max이후로 돌아가지 못하게 처리가 되어 있는 듯하다. 예외처리 필요X
 skipButtons.forEach((skipBtn) =>
   skipBtn.addEventListener("click", function () {
     let skipper = Number(this.dataset.skip);
-    if (myVideo.currentTime + skipper < 0) {
-      myVideo.currentTime = 0;
-    } else {
-      myVideo.currentTime += skipper;
-    }
-    console.log(myVideo.currentTime);
+    myVideo.currentTime += skipper;
   })
 );
 
 myVideo.addEventListener("timeupdate", function () {
   const barRatio = (myVideo.currentTime / durationTime) * 100;
-  console.log(barRatio);
   progressContent.style.flexBasis = `${barRatio}%`;
 });
 
@@ -58,7 +60,9 @@ let isDown = false;
 progress.addEventListener("mousedown", () => (isDown = true));
 progress.addEventListener("mousemove", (e) => {
   if (isDown) {
-    myVideo.currentTime = e.offsetX;
+    //const scrubTime= e.offsetX;
+    const scrubTime = (e.offsetX / progress.offsetWidth) * myVideo.duration;
+    myVideo.currentTime = scrubTime;
   }
 });
 progress.addEventListener("mouseup", (e) => {
