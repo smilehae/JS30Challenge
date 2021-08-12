4925b04929464aeedb2ce0d3abfc1484f7c37994
smilehae <mihaepyun@gmail.com>
2021-08-12 14:33:48 +0900

    현재 시간에 맞게 진행바 이동 완료 : timeupdate를 이벤트리스터에 사용하면 currentTime이 바뀔 때 마다 반영된다. duration에 총 비디오의 길이를 가져올 수 있고, loadmetadata를 통해서 가져오면 된다.

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,8 +3,16 @@ const playBtn = player.querySelector(".toggle");
 const myVideo = player.querySelector("video.viewer");
 const volumeRange = player.querySelector("input[name=volume]");
 const playBackRange = player.querySelector("input[name=playbackRate]");
+//이렇게 [속성] 만으로도 찾기 가능
+const skipButtons = player.querySelectorAll("[data-skip]");
+const progress = player.querySelector(".progress");
+const progressContent = player.querySelector(".progress__filled");
 
-console.log(playBackRange.value);
+let durationTime;
+
+myVideo.addEventListener("loadedmetadata", function () {
+  durationTime = myVideo.duration;
+});
 
 playBtn.addEventListener("click", function (e) {
   if (myVideo.paused) {
@@ -22,4 +30,22 @@ volumeRange.addEventListener("input", function () {
 
 playBackRange.addEventListener("input", function () {
   myVideo.playbackRate = this.value;
+});
+
+skipButtons.forEach((skipBtn) =>
+  skipBtn.addEventListener("click", function () {
+    let skipper = Number(this.dataset.skip);
+    if (myVideo.currentTime + skipper < 0) {
+      myVideo.currentTime = 0;
+    } else {
+      myVideo.currentTime += skipper;
+    }
+    console.log(myVideo.currentTime);
+  })
+);
+
+myVideo.addEventListener("timeupdate", function () {
+  const barRatio = (myVideo.currentTime / durationTime) * 100;
+  console.log(barRatio);
+  progressContent.style.flexBasis = `${barRatio}%`;
 });
