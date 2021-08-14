f7e17b1c04db1cdbbab8cbeb7b015582f90e6b85
smilehae <mihaepyun@gmail.com>
2021-08-14 15:36:09 +0900

    플젝 20. 목소리 인식 인강 들음

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,19 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+function getVideo() {
+  const MediaStream = new MediaStream();
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((MediaStream) => {
+      console.log(MediaStream);
+      //비디오가 이해하는 형식으로 만들어짐
+      video.src = window.URL.createObjectURL(MediaStream);
+      video.play();
+    });
+}
+
+getVideo();
